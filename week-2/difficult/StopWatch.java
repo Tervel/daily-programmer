b9ade8209969db073d2def6c3c8ae937ba875f34
RekN <jordananslow@gmail.com>
2015-06-16 23:46:45 +1000

    Progress on StopWatch

--- week-2/difficult/StopWatch.java
+++ week-2/difficult/StopWatch.java
@@ -10,7 +10,8 @@ public class StopWatch {
 		System.out.println("Choose an option:");
 		System.out.println("1. Start");
 		System.out.println("2. Stop");
-		System.out.println("3. Lap\n");
+		System.out.println("3. Lap");
+		System.out.println("4. Display time\n");
 		System.out.print(" >>  ");
 
 		selection = input.nextInt();
@@ -19,18 +20,33 @@ public class StopWatch {
 	}
 
 	public static void main(String[] args) throws Exception {
-		int choice = mainMenu();
+		long startTime = 0;
+		
 
-		switch (choice) {
-			case 1:
-				// Start
-				break;
-			case 2:
-				// Stop
-				break;
-			case 3:
-				// Lap
-				break;
+		while(true) {
+			int choice = mainMenu();
+			long elapsedTime = System.currentTimeMillis() - startTime;
+			long elapsedMilli = elapsedTime / 100;
+			long elapsedSeconds = elapsedTime / 1000;
+			long secondsDisplay = elapsedSeconds % 60;
+			long elapsedMinutes = elapsedSeconds / 60;
+
+			switch (choice) {
+				case 1:
+					startTime = System.currentTimeMillis();
+					break;
+				case 2:
+					break;
+				case 3:
+					// Lap: store elapsed time in array
+					break;
+				case 4:
+					System.out.printf("\nTime elapsed: %d:%d:%d\n", elapsedMinutes, elapsedSeconds, elapsedMilli);
+					break;
+				default:
+					// null
+					break;
+			}
 		}
 	}
 }
