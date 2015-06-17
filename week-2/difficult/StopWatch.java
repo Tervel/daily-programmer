f175e49fe444817242ed1f60ab882df11aa561ff
RekN <jordananslow@gmail.com>
2015-06-17 16:41:47 +1000

    Week 2: StopWatch

--- week-2/difficult/StopWatch.java
+++ week-2/difficult/StopWatch.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -7,7 +8,7 @@ public class StopWatch {
 		int selection;
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("Choose an option:");
+		System.out.println("\nChoose an option:");
 		System.out.println("1. Start");
 		System.out.println("2. Stop");
 		System.out.println("3. Lap");
@@ -21,12 +22,13 @@ public class StopWatch {
 
 	public static void main(String[] args) throws Exception {
 		long startTime = 0;
+		ArrayList<String> lapTimes = new ArrayList<String>();
 		
-
 		while(true) {
 			int choice = mainMenu();
 			long elapsedTime = System.currentTimeMillis() - startTime;
 			long elapsedMilli = elapsedTime / 100;
+			long milliDisplay = elapsedTime % 60;
 			long elapsedSeconds = elapsedTime / 1000;
 			long secondsDisplay = elapsedSeconds % 60;
 			long elapsedMinutes = elapsedSeconds / 60;
@@ -36,12 +38,23 @@ public class StopWatch {
 					startTime = System.currentTimeMillis();
 					break;
 				case 2:
+					// Reset our time and all lap times
+					startTime = 0;
+					lapTimes.clear();
 					break;
 				case 3:
 					// Lap: store elapsed time in array
+					lapTimes.add(String.format("Lap: %d:%d", secondsDisplay, milliDisplay));
 					break;
 				case 4:
-					System.out.printf("\nTime elapsed: %d:%d:%d\n", elapsedMinutes, elapsedSeconds, elapsedMilli);
+					if (startTime != 0) {
+						System.out.printf("\nTime elapsed: %d:%d:%d\n", elapsedMinutes, secondsDisplay, milliDisplay);
+						for (int i = 0; i < lapTimes.size(); i++) {
+							System.out.println(lapTimes.get(i));
+						}
+					} else {
+						System.out.println("0");
+					}
 					break;
 				default:
 					// null
