a162ad24f86d402b33f6cad5c3321047c60d8e84
Jordan Anslow <jordananslow@gmail.com>
2015-06-24 01:53:50 +1000

    Cleaned up formatting

--- week-4/easy/easy.py
+++ week-4/easy/easy.py
@@ -1,30 +1,29 @@
 import random
 
 def getUserInput():
-	print ("Hello, please enter the length of the password you wish to be generated:")
-	userInput = input(" >>  ")
+    print ("Hello, please enter the length of the password you wish to be generated:")
+    userInput = input(" >>  ")
 
-	return int(userInput)
+    return int(userInput)
 
 def generatePassword(userInput):
-	passwordLength = userInput
-	password = ""
+    passwordLength = userInput
+    password = ""
 
-	# Generate our password with given length
-	for i in range(0, passwordLength):
-		# Modulo by 93 + 33 to produce the range of ASCII characters we want for the password
-		password += chr(random.getrandbits(56) % 93 + 33)
+    # Generate our password with given length
+    for i in range(0, passwordLength):
+        # Modulo by 93 + 33 to produce the range of ASCII characters we want for the password
+        password += chr(random.getrandbits(56) % 93 + 33)
 
-	return password
+    return password
 
 def main():
-	userInput = getUserInput()
-	generatedPassword = generatePassword(userInput)
+    userInput = getUserInput()
+    generatedPassword = generatePassword(userInput)
 
-	print (generatedPassword)
-	return
+    print (generatedPassword)
+    return
 
 if __name__ == "__main__":
-	main()
+    main()
 
-	
