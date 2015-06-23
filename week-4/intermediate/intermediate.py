82dcc7f8237e1376b6ee946923ce575f325e870a
Jordan Anslow <jordananslow@gmail.com>
2015-06-24 01:53:25 +1000

    Progress on Week 4: intermediate

--- week-4/intermediate/intermediate.py
+++ week-4/intermediate/intermediate.py
@@ -6,8 +6,25 @@
 
 import re
 
-operatorRegex = '[+, \-, *, /, (, )]+'
+operations = {
+    "+" : lambda a, b: a + b,
+    "*" : lambda a, b: a * b,
+    "-" : lambda a, b: a - b,
+    "/" : lambda a, b: a / b,
+}
+
+operatorRegex = '[+, \-, *, /]+'
+separatorRegex = '[(, )]+'
 numRegex = '[0-9]+'
+
+def precedence(operator):
+    return {
+        '^': 4,
+        '*': 3,
+        '/': 3,
+        '+': 2,
+        '-': 2,
+    }.get(operator, 'Error') # Default
 
 def getUserInput():
     print ("Please enter an equation:")
@@ -20,15 +37,42 @@ def calculate(userInput):
 
     operands = re.findall(numRegex, string)
     operators = []
+    tokenList = []
 
-    # Iterate though userinput, push each operator to the stack
+    output = []
+    stack = []
+
+    # Tokenize input
     for character in string:
         operatorChar = re.match(operatorRegex, character)
         if operatorChar != None:
             operators.append(character)
 
-    print (operands)
-    print (operators)
+    # Shunting yard
+    tokenList = operands + operators
+    for token in tokenList: # read a token
+        numberToken = re.match(numRegex, token)
+        operatorToken = re.match(operatorRegex, token)
+
+        if numberToken != None: # if token is a number, add to output
+            output.append(token)
+        elif operatorToken != None: # if token is an operator
+            o1 = token
+            while len(stack) and precedence(o1) <= precedence(stack[-1]): # while there are operators in stack
+                output.append(stack.pop()) # pop top of stack, and push to output
+            stack.append(o1) # push o1 onto stack
+        elif token == '(':
+            stack.append(token)
+        elif token == ')':
+            while not len(stack) and stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop() # pop '(' from stack
+
+    while len(stack): # while stack not empty
+        output.append(stack.pop())
+
+    print (output)
+    print (stack)
 
     return
 
