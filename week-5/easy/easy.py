ad7633dec17293099caee55206cd324077d98e32
Jordan Anslow <jordananslow@gmail.com>
2015-06-30 05:18:14 +1000

    Changed folder name

---
+++ week-5/easy/easy.py
@@ -0,0 +1,13 @@
+def getInput():
+    print("Enter calculation: ")
+    userinput = input(" >>  ")
+
+    return userinput
+
+def main():
+    
+
+    return
+
+if __name__ == "__main__":
+    main()
