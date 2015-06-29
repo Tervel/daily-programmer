71e094c71c2e517654d04c8499fa034a687d77c9
Jordan Anslow <jordananslow@gmail.com>
2015-06-30 05:17:47 +1000

    Week 4: difficult

---
+++ week-4/difficult/difficult.py
@@ -0,0 +1,43 @@
+import itertools
+import re
+
+numRegex = '[0-9]+'
+operators = {
+    "+" : lambda a, b: a + b,
+    "*" : lambda a, b: a * b,
+    "-" : lambda a, b: a - b,
+    "/" : lambda a, b: a / b,
+    "%" : lambda a, b: a % b,
+}
+
+def getInput():
+    print("Enter three separate numbers, eg; 1, 2, 3: ")
+    userinput = input(" >>  ")
+
+    return userinput
+
+def applyOperator(op, val1, val2):
+    return operators[op](val1, val2)
+
+def main():
+    results = []
+
+    userinput = getInput()
+    numbers = re.findall(numRegex, userinput)
+    inputPermutations = list(itertools.permutations([numbers[0], numbers[1], numbers[2]]))
+
+    # for each operator, iterate through all combinations of x, y, z
+    # eg: 1, 2, 3 > 1 + 2 = 3
+    #               3 - 2 = 1
+    #               ..... = ...
+    # if x, y = z then print the operation + combination
+    for operation in operators:
+        for permutation in inputPermutations:
+            result = applyOperator(operation, int(permutation[0]), int(permutation[1]))
+            if float(permutation[2]) == float(result):
+                print ("{0} {1} {2} = {3}".format(permutation[0], operation, permutation[1], result))
+
+    return
+
+if __name__ == "__main__":
+    main()
